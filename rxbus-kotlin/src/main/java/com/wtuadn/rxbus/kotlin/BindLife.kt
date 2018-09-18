a60cea1be1a224cf32d63c1bb923418c551354d1
wtuadn <wtuadn@gmail.com>
2018-09-18 14:19:13 +0800

    1.0.3 bindLife支持自定义解除绑定的生命周期

--- rxbus-kotlin/src/main/java/com/wtuadn/rxbus/kotlin/BindLife.kt
+++ rxbus-kotlin/src/main/java/com/wtuadn/rxbus/kotlin/BindLife.kt
@@ -1,23 +1,33 @@
 package com.wtuadn.rxbus.kotlin
 
+import android.arch.lifecycle.Lifecycle
 import android.arch.lifecycle.LifecycleOwner
 import com.wtuadn.rxbus.RxUtils
 import io.reactivex.Flowable
 import io.reactivex.Maybe
 import io.reactivex.Observable
 import io.reactivex.Single
+import io.reactivex.disposables.Disposable
 
 /**
  * Created by wtuadn on 2018/6/6.
  */
-inline fun <T> Observable<T>.bindLife(o: LifecycleOwner, noinline c: (t: T) -> Unit) =
-        this.`as`(RxUtils.bindLifecycle(o)).subscribe(c)
+inline fun <T> Observable<T>.bindLife(o: LifecycleOwner, untilEvent: Lifecycle.Event? = null, noinline c: (t: T) -> Unit): Disposable {
+    return if (untilEvent == null) `as`(RxUtils.bindLifecycle(o)).subscribe(c)
+    else `as`(RxUtils.bindLifecycle(o, untilEvent)).subscribe(c)
+}
 
-inline fun <T> Single<T>.bindLife(o: LifecycleOwner, noinline c: (t: T) -> Unit) =
-        this.`as`(RxUtils.bindLifecycle(o)).subscribe(c)
+inline fun <T> Single<T>.bindLife(o: LifecycleOwner, untilEvent: Lifecycle.Event? = null, noinline c: (t: T) -> Unit): Disposable {
+    return if (untilEvent == null) `as`(RxUtils.bindLifecycle(o)).subscribe(c)
+    else `as`(RxUtils.bindLifecycle(o, untilEvent)).subscribe(c)
+}
 
-inline fun <T> Maybe<T>.bindLife(o: LifecycleOwner, noinline c: (t: T) -> Unit) =
-        this.`as`(RxUtils.bindLifecycle(o)).subscribe(c)
+inline fun <T> Maybe<T>.bindLife(o: LifecycleOwner, untilEvent: Lifecycle.Event? = null, noinline c: (t: T) -> Unit): Disposable {
+    return if (untilEvent == null) `as`(RxUtils.bindLifecycle(o)).subscribe(c)
+    else `as`(RxUtils.bindLifecycle(o, untilEvent)).subscribe(c)
+}
 
-inline fun <T> Flowable<T>.bindLife(o: LifecycleOwner, noinline c: (t: T) -> Unit) =
-        this.`as`(RxUtils.bindLifecycle(o)).subscribe(c)
+inline fun <T> Flowable<T>.bindLife(o: LifecycleOwner, untilEvent: Lifecycle.Event? = null, noinline c: (t: T) -> Unit): Disposable {
+    return if (untilEvent == null) `as`(RxUtils.bindLifecycle(o)).subscribe(c)
+    else `as`(RxUtils.bindLifecycle(o, untilEvent)).subscribe(c)
+}
--- rxbus/src/main/java/com/wtuadn/rxbus/RxUtils.java
+++ rxbus/src/main/java/com/wtuadn/rxbus/RxUtils.java
@@ -1,5 +1,6 @@
 package com.wtuadn.rxbus;
 
+import android.arch.lifecycle.Lifecycle;
 import android.arch.lifecycle.LifecycleOwner;
 
 import com.uber.autodispose.AutoDispose;
@@ -9,5 +10,9 @@ import com.uber.autodispose.android.lifecycle.AndroidLifecycleScopeProvider;
 public class RxUtils {
     public static <T> AutoDisposeConverter<T> bindLifecycle(LifecycleOwner lifecycleOwner) {
         return AutoDispose.autoDisposable(AndroidLifecycleScopeProvider.from(lifecycleOwner));
+    }
+
+    public static <T> AutoDisposeConverter<T> bindLifecycle(LifecycleOwner lifecycleOwner, Lifecycle.Event untilEvent) {
+        return AutoDispose.autoDisposable(AndroidLifecycleScopeProvider.from(lifecycleOwner, untilEvent));
     }
 }
